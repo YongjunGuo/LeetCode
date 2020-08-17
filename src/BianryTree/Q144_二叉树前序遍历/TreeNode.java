b837d78506cb592f45cb0159c11dec66b1fdc246
gooyj@tju.edu.cn <gooyj@tju.edu.cn>
2020-08-17 21:37:04 +0800

    ...

---
+++ src/BianryTree/Q144_二叉树前序遍历/TreeNode.java
@@ -0,0 +1,11 @@
+package BianryTree.Q144_二叉树前序遍历;
+
+public class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int x){
+        val = x;
+    }
+}
---
+++ src/BianryTree/Q144_二叉树前序遍历/solution.java
@@ -0,0 +1,4 @@
+package BianryTree.Q144_二叉树前序遍历;
+
+public class solution {
+}
