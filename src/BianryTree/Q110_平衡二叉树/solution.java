bce4c52767db2812ba07837ce25f54282b7f8ae9
gooyj@tju.edu.cn <gooyj@tju.edu.cn>
2020-08-17 21:31:45 +0800

    first commit

---
+++ src/BianryTree/Q110_平衡二叉树/solution.java
@@ -0,0 +1,33 @@
+package BianryTree.Q110_平衡二叉树;
+
+public class solution {
+    public boolean isBalanced(TreeNode root) {
+        return height(root) >= 0;
+    }
+    public int height(TreeNode root){
+        if(root == null){
+            return 0;
+        }
+
+        int leftHeight = height(root.left);
+        int rightHeight = height(root.right);
+        if(leftHeight == -1 || rightHeight == -1 || Math.abs(leftHeight - rightHeight) > 1 ){
+            return -1;
+        }else{
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+    }
+
+    public static void main(String[] args){
+        TreeNode root = new TreeNode(3);
+        TreeNode n1 = new TreeNode(9);
+        TreeNode n2 = new TreeNode(20);
+        TreeNode n3 = new TreeNode(15);
+        TreeNode n4 = new TreeNode(7);
+        root.left = n1;
+        root.right = n2;
+        n2.left = n3;
+        n2.right = n4;
+        System.out.println(new solution().isBalanced(root));
+    }
+}
