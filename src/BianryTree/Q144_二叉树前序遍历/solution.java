7df832939eabc3ca9a5a89e8c7ef68a3916ea6b5
YongjunGuo <gooyj@tju.edu.cn>
2020-09-04 16:23:26 +0800

    二叉树前序遍历

--- src/BianryTree/Q144_二叉树前序遍历/solution.java
+++ src/BianryTree/Q144_二叉树前序遍历/solution.java
@@ -1,4 +1,42 @@
 package BianryTree.Q144_二叉树前序遍历;
 
+/*
+* 给定一个二叉树，返回它的 前序 遍历
+* */
+
+import java.util.*;
 public class solution {
+    public List<Integer> preorderTravelsal(TreeNode root){
+        LinkedList<TreeNode> stack = new LinkedList<>();
+        LinkedList<Integer> output = new LinkedList<>();
+        if(root == null){
+            return output;
+        }
+        stack.add(root);
+        while(!stack.isEmpty()){
+            TreeNode node = stack.pollLast();
+            output.add(node.val);
+            if(node.left != null){
+                stack.add(node.left);
+            }
+            if(node.right != null){
+                stack.add(node.right);
+            }
+        }
+        return output;
+    }
+    public static void main(String [] args){
+        TreeNode root = new TreeNode(1);
+        TreeNode n1 = new TreeNode(2);
+        TreeNode n2 = new TreeNode(3);
+        TreeNode n3 = new TreeNode(5);
+
+        root.left = n1;
+        root.right = n2;
+        n1.right = n3;
+
+        System.out.println(new solution().preorderTravelsal(root));
+
+    }
+
 }
