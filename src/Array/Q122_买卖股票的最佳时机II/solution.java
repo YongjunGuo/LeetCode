e59ebeca96e6bd695cca8618df87dd416e523beb
YongjunGuo <gooyj@tju.edu.cn>
2020-09-08 21:40:21 +0800

    update

--- src/Array/Q122_买卖股票的最佳时机II/solution.java
+++ src/Array/Q122_买卖股票的最佳时机II/solution.java
@@ -1,9 +1,19 @@
 package Array.Q122_买卖股票的最佳时机II;
 
+/*
+* 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+* 设计一个算法来计算你所能获取的最大利润。你可以尽可能地完成更多的交易（多次买卖一支股票）。
+* 注意：你不能同时参与多笔交易（你必须在再次购买前出售掉之前的股票）。
+* */
+
 import java.util.Scanner;
+
 
 public class solution {
     public int maxProfit(int[] prices){
+        /*
+        * 我们可以直接继续增加加数组的连续数字之间的差值，如果第二个数字大于第一个数字，我们获得的总和将是最大利润。
+        * */
         int maxprofit = 0;
         for(int i = 1; i < prices.length; i++){
             if (prices[i] > prices[i - 1]){
