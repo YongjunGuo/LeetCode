f7021c3bdae517fe418e21ceaed8cceab4808be7
gooyj <gootj@tju.edu.cn>
2021-03-16 22:43:26 +0800

    公共类

---
+++ src/LinkedList/common.java
@@ -0,0 +1,15 @@
+package LinkedList;
+
+
+
+public class common {
+    public static class ListNode {
+        ListNode next;
+        int val;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+}
